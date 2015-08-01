b95f95c86948abf0d909abc730d410f13d4fbbf6
dpg5000 <dgoussev@gmail.com>
2015-07-31 22:55:47 -0700

    assignment-32-seed completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,21 +2,37 @@
 require 'faker'
 
 #create test posts
-50.times do
-  Post.create!(
-    title: Faker::Lorem.sentence,
-    body: Faker::Lorem.paragraph
-  )
-end
-posts = Post.all
+# 50.times do
+#   Post.create!(
+#     title: Faker::Lorem.sentence,
+#     body: Faker::Lorem.paragraph
+#   )
+# end
+# posts = Post.all
+#
+# #create test commments
+# 100.times do
+#   Comment.create!(
+#     post: posts.sample,
+#     body: Faker::Lorem.paragraph
+#   )
+# end
 
-#create test commments
-100.times do
-  Comment.create!(
-    post: posts.sample,
-    body: Faker::Lorem.paragraph
-  )
-end
+  # Post.create(
+  #   title: "DPG unique title 2",
+  #   body: "this is the most amazingly unique post body ever!"
+  # )
+
+  Post.where(
+      title: "Unique Title!",
+      body: "this is the most amazingly unique post body ever!"
+    ).first_or_create
+
+
+  Comment.where(
+      post: Post.where(title: "Unique Title!"),
+      body: "unique comment!"
+    ).first_or_create
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
