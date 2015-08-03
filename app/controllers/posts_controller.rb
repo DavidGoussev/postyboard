fe5dcda87431b435e2f13989e9666280a32d064c
dpg5000 <dgoussev@gmail.com>
2015-08-02 17:19:12 -0700

    Checkpoint35 completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,33 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :body))
+    if @post.save
+      flash[:notice] = "post was saved!"
+      redirect_to @post
+    else
+      flash[:error] = "there was an error saving your post. please try again!"
+      render :new
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(params.require(:post).permit(:title, :body))
+      flash[:notice] = "post was updated!"
+      redirect_to @post
+    else
+      flash[:error] = "post was not updated, please try again"
+      render :edit
+    end
+  end
+
 end
