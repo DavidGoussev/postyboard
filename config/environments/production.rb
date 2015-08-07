fcd282228e906c03f05bdda399c0a1af0da546ba
dpg5000 <dgoussev@gmail.com>
2015-08-07 16:26:13 -0700

    configured app for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,12 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # http://bloccit.herokuapp.com
+  config.action_mailer.default_url_options = { host: 'dpg5000-bloccit.herokuapp.com' }
+
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+ # config.force_ssl = true
+ config.force_ssl = true
+
 end
