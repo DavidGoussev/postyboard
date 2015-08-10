893c0b2e06b447c7b76cbc62d34a240f8aeb8459
dpg5000 <dgoussev@gmail.com>
2015-08-09 20:20:22 -0700

    assignment-43-markdown completed3

--- app/models/post.rb
+++ app/models/post.rb
@@ -24,18 +24,18 @@ class Post < ActiveRecord::Base
   validates :user, presence: true
 
   def markdown_title
-    render_as_markdown(self.title)
+    render_as_markdown(title)
   end
 
   def markdown_body
-    render_as_markdown(self.body)
+    render_as_markdown(body)
   end
 
   private
 
   def render_as_markdown(markdown)
     renderer = Redcarpet::Render::HTML.new
-    extensions = {fenced_code_blocks: true}
+    extensions = {fenced_code_blocks: true, autolink: true}
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render markdown).html_safe
   end
